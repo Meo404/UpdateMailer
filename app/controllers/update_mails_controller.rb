63a45fa87b33a5f735d861bc8de7b308be505b77
Maurice Vogel <synonn@web.de>
2016-06-18 11:49:46 +0200

    changed update mail sorting to put null values last - this allows for correct sent_at filtering

--- app/controllers/update_mails_controller.rb
+++ app/controllers/update_mails_controller.rb
@@ -5,7 +5,8 @@ class UpdateMailsController < ApplicationController
   before_action :allowed_to_view, only: [:view]
 
   def index
-    @update_mails = UpdateMail.search(params[:search], current_user).order(sort_column + ' ' + sort_direction('desc'))
+    @update_mails = UpdateMail.search(params[:search], current_user)
+                              .order(sort_column + ' ' + sort_direction('desc') + ' NULLS LAST')
                               .paginate(page: params[:page], per_page: 25)
   end
 
